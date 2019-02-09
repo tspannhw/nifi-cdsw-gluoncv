4e6d4eac0407eb6ba94fa3ba9076c5af47784552
Timothy Spann <tspann@hortonworks.com>
2019-02-08 19:31:57 -0500

    Update cdsw-build.sh

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -1,10 +1,10 @@
+pip2 install mxnet
+pip3 install mxnet
+pip2 install psutil
+pip3 install psutil
 pip3 install sklearn
 pip2 install sklearn
 pip2 install textblob
 pip3 install textblob
 pip2 install gluoncv
 pip3 install gluoncv
-pip2 install mxnet
-pip3 install mxnet
-pip2 install psutil
-pip3 install psutil
